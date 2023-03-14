bdf8818c9bc0e6d81bbd0f831b90230e39754472
Arturo Alva <124171994+ArtuIVO@users.noreply.github.com>
2023-03-14 09:47:29 -0600

    Parte de Arturo
    Inicio del apartado de las variables

---
+++ mian.py
@@ -0,0 +1,14 @@
+#Trabajo de Arturo
+#Entrada
+
+n1 = int(input('Ingrese un primero número: '))
+n2 = int(input('Ingrese un segundo número: '))
+
+#Proceso
+
+if n1 > n2:
+    n3 = n1
+    print('El número mayor es: ', n3)
+else:
+    n3 = n2
+    print('El número mayor es: ', n3)

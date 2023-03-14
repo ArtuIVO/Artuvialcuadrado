915a4d670884a7a22d9dbca4805e08d63505b3f1
Arthur Yoshioka <ahyoshioka@correo.url.edu.gt>
2023-03-14 09:52:30 -0600

    Update mian.py
    Parte dos del programa

---
+++ mian.py
@@ -0,0 +1,10 @@
+# Parte 2 por Arthur Yoshioka
+
+n3 = int(input("Ingrese el pirmer numero"))
+
+b = a % 2
+
+if c ==  0:
+    print("Es un numero par porque tiene residuo 0")
+else:
+    print("Es un numero impar porque no tiene residuo 0")
